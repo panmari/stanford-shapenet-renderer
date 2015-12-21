a7b6769587c39420310c508615f91e1bd97289bd
panmari <smmuzi@gmail.com>
2015-12-21 12:09:01 +0100

    Also dumping normals and albedo, added a second light source.

--- render_blender.py
+++ render_blender.py
@@ -21,10 +21,15 @@ f = args.obj
 import bpy
 from mathutils import Vector
 
+
 # Set up rendering of depth map:
 bpy.context.scene.use_nodes = True
 tree = bpy.context.scene.node_tree
 links = tree.links
+
+# Add passes for additionally dumping albed and normals.
+bpy.context.scene.render.layers["RenderLayer"].use_pass_normal = True
+bpy.context.scene.render.layers["RenderLayer"].use_pass_color = True
 
 # clear default nodes
 for n in tree.nodes:
@@ -35,19 +40,43 @@ rl = tree.nodes.new('CompositorNodeRLayers')
 
 map = tree.nodes.new(type="CompositorNodeMapValue")
 # Size is chosen kind of arbitrarily, try out until you're satisfied with resulting depth map.
-map.size = [0.5]
+map.offset = [-0.7]
+map.size = [1.4]
 map.use_min = True
 map.min = [0]
 map.use_max = True
 map.max = [255]
-links.new(rl.outputs[2], map.inputs[0])
+links.new(rl.outputs['Z'], map.inputs[0])
 
 invert = tree.nodes.new(type="CompositorNodeInvert")
 links.new(map.outputs[0], invert.inputs[1])
 
 # create a file output node and set the path
 depthFileOutput = tree.nodes.new(type="CompositorNodeOutputFile")
+depthFileOutput.label = 'Depth Output'
 links.new(invert.outputs[0], depthFileOutput.inputs[0])
+
+scale_normal = tree.nodes.new(type="CompositorNodeMixRGB")
+scale_normal.blend_type = 'MULTIPLY'
+#scale_normal.use_alpha = True
+scale_normal.inputs[2].default_value = (0.5, 0.5, 0.5, 1)
+links.new(rl.outputs['Normal'], scale_normal.inputs[1])
+
+bias_normal = tree.nodes.new(type="CompositorNodeMixRGB")
+bias_normal.blend_type = 'ADD'
+#bias_normal.use_alpha = True
+bias_normal.inputs[2].default_value = (0.5, 0.5, 0.5, 0)
+links.new(scale_normal.outputs[0], bias_normal.inputs[1])
+
+
+normalFileOutput = tree.nodes.new(type="CompositorNodeOutputFile")
+normalFileOutput.label = 'Normal Output'
+links.new(bias_normal.outputs[0], normalFileOutput.inputs[0])
+
+albedoFileOutput = tree.nodes.new(type="CompositorNodeOutputFile")
+albedoFileOutput.label = 'Albedo Output'
+# For some reason,
+links.new(rl.outputs['Color'], albedoFileOutput.inputs[0])
 
 # Delete default cube
 bpy.data.objects['Cube'].select = True
@@ -72,6 +101,15 @@ lamp.type = 'SUN'
 lamp.shadow_method = 'NOSHADOW'
 # Possibly disable specular shading:
 lamp.use_specular = False
+
+# Add another light source so stuff facing away from light is not completely dark
+bpy.ops.object.lamp_add(type='SUN')
+lamp2 = bpy.data.lamps['Sun']
+lamp2.shadow_method = 'NOSHADOW'
+lamp2.use_specular = False
+lamp2.energy = 0.015
+bpy.data.objects['Sun'].rotation_euler = bpy.data.objects['Lamp'].rotation_euler
+bpy.data.objects['Sun'].rotation_euler[0] += 180
 
 def parent_obj_to_camera(b_camera):
     origin = (0,0,0)
@@ -105,13 +143,18 @@ from math import radians
 
 stepsize = 360.0 / args.views
 rotation_mode = 'XYZ'
-depthFileOutput.base_path = ''
+
+for output_node in [depthFileOutput, normalFileOutput, albedoFileOutput]:
+    output_node.base_path = ''
 
 for i in range(0, args.views):
     print("Rotation {}, {}".format((stepsize * i), radians(stepsize * i)))
 
     scene.render.filepath = fp + '_r_{0:03d}'.format(int(i * stepsize))
     depthFileOutput.file_slots[0].path = scene.render.filepath + "_depth.png"
+    normalFileOutput.file_slots[0].path = scene.render.filepath + "_normal.png"
+    albedoFileOutput.file_slots[0].path = scene.render.filepath + "_albedo.png"
+
     bpy.ops.render.render(write_still=True) # render still
 
     b_empty.rotation_euler[2] += radians(stepsize)
