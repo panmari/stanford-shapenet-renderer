5d2ae775331f39aa1604ef1bfc6504d6f7249dd3
Markus Völk <markus.voelk@gmx.de>
2020-12-22 21:20:23 +0100

    Update to Blender 2.9

--- render_blender.py
+++ render_blender.py
@@ -1,11 +1,15 @@
 # A simple script that uses blender to render views of a single object by rotation the camera around it.
 # Also produces depth map at the same time.
 #
+# Tested with Blender 2.9
+#
 # Example:
 # blender --background --python mytest.py -- --views 10 /path/to/my.obj
 #
 
-import argparse, sys, os
+import argparse, sys, os, math
+import bpy
+from glob import glob
 
 parser = argparse.ArgumentParser(description='Renders given obj file by rotation a camera around it.')
 parser.add_argument('--views', type=int, default=30,
@@ -26,149 +30,184 @@ parser.add_argument('--color_depth', type=str, default='8',
                     help='Number of bit per channel used for output. Either 8 or 16.')
 parser.add_argument('--format', type=str, default='PNG',
                     help='Format of files generated. Either PNG or OPEN_EXR')
+parser.add_argument('--resolution', type=int, default=600,
+                    help='Resolution of the images.')
 
 argv = sys.argv[sys.argv.index("--") + 1:]
 args = parser.parse_args(argv)
 
-import bpy
+# Set up rendering
+context = bpy.context
+scene = bpy.context.scene
+render = bpy.context.scene.render
 
-# Set up rendering of depth map.
+scene.view_layers["View Layer"].use_pass_normal = True
+scene.view_layers["View Layer"].use_pass_diffuse_color = True
+
+render.engine = 'CYCLES' # ('CYCLES', 'BLENDER_EEVEE', ...)
+render.image_settings.color_mode = 'RGBA' # ('RGB', 'RGBA', ...)
+render.image_settings.color_depth = args.color_depth # ('8', '16')
+render.image_settings.file_format = args.format # ('PNG', 'OPEN_EXR', 'JPEG, ...)
+render.resolution_x = args.resolution
+render.resolution_y = args.resolution
+render.resolution_percentage = 100
+render.film_transparent = True
+
 bpy.context.scene.use_nodes = True
-tree = bpy.context.scene.node_tree
-links = tree.links
-
-# Add passes for additionally dumping albedo and normals.
-bpy.context.scene.render.layers["RenderLayer"].use_pass_normal = True
-bpy.context.scene.render.layers["RenderLayer"].use_pass_color = True
-bpy.context.scene.render.image_settings.file_format = args.format
-bpy.context.scene.render.image_settings.color_depth = args.color_depth
+nodes = bpy.context.scene.node_tree.nodes
+links = bpy.context.scene.node_tree.links
 
 # Clear default nodes
-for n in tree.nodes:
-    tree.nodes.remove(n)
+for n in nodes:
+    nodes.remove(n)
 
-# Create input render layer node.
-render_layers = tree.nodes.new('CompositorNodeRLayers')
+# Create input render layer node
+render_layers = nodes.new('CompositorNodeRLayers')
 
-depth_file_output = tree.nodes.new(type="CompositorNodeOutputFile")
+# Create depth output nodes
+depth_file_output = nodes.new(type="CompositorNodeOutputFile")
 depth_file_output.label = 'Depth Output'
 if args.format == 'OPEN_EXR':
-  links.new(render_layers.outputs['Depth'], depth_file_output.inputs[0])
+    depth_file_output.format.file_format = 'OPEN_EXR'
+    links.new(render_layers.outputs['Depth'], depth_file_output.inputs[0])
 else:
-  # Remap as other types can not represent the full range of depth.
-  map = tree.nodes.new(type="CompositorNodeMapValue")
-  # Size is chosen kind of arbitrarily, try out until you're satisfied with resulting depth map.
-  map.offset = [-0.7]
-  map.size = [args.depth_scale]
-  map.use_min = True
-  map.min = [0]
-  links.new(render_layers.outputs['Depth'], map.inputs[0])
+    depth_file_output.format.file_format = "PNG"
+    depth_file_output.format.color_mode = "BW"
+    depth_file_output.format.color_depth = '16'
+    # Remap as other types can not represent the full range of depth.
+    map = nodes.new(type="CompositorNodeMapValue")
+    # Size is chosen kind of arbitrarily, try out until you're satisfied with resulting depth map.
+    map.offset = [-0.7]
+    map.size = [args.depth_scale]
+    map.use_min = True
+    map.min = [0]
+    links.new(render_layers.outputs['Depth'], map.inputs[0])
+    links.new(map.outputs[0], depth_file_output.inputs[0])
 
-  links.new(map.outputs[0], depth_file_output.inputs[0])
-
-scale_normal = tree.nodes.new(type="CompositorNodeMixRGB")
+# Create normal output nodes
+scale_normal = nodes.new(type="CompositorNodeMixRGB")
 scale_normal.blend_type = 'MULTIPLY'
 # scale_normal.use_alpha = True
 scale_normal.inputs[2].default_value = (0.5, 0.5, 0.5, 1)
 links.new(render_layers.outputs['Normal'], scale_normal.inputs[1])
 
-bias_normal = tree.nodes.new(type="CompositorNodeMixRGB")
+bias_normal = nodes.new(type="CompositorNodeMixRGB")
 bias_normal.blend_type = 'ADD'
 # bias_normal.use_alpha = True
 bias_normal.inputs[2].default_value = (0.5, 0.5, 0.5, 0)
 links.new(scale_normal.outputs[0], bias_normal.inputs[1])
 
-normal_file_output = tree.nodes.new(type="CompositorNodeOutputFile")
+normal_file_output = nodes.new(type="CompositorNodeOutputFile")
 normal_file_output.label = 'Normal Output'
+normal_file_output.format.file_format = args.format
 links.new(bias_normal.outputs[0], normal_file_output.inputs[0])
 
-albedo_file_output = tree.nodes.new(type="CompositorNodeOutputFile")
+# Create albedo output nodes
+alpha_albedo = nodes.new(type="CompositorNodeSetAlpha")
+links.new(render_layers.outputs['DiffCol'], alpha_albedo.inputs['Image'])
+links.new(render_layers.outputs['Alpha'], alpha_albedo.inputs['Alpha'])
+
+albedo_file_output = nodes.new(type="CompositorNodeOutputFile")
 albedo_file_output.label = 'Albedo Output'
-links.new(render_layers.outputs['Color'], albedo_file_output.inputs[0])
+albedo_file_output.format.file_format = args.format
+albedo_file_output.format.color_mode = 'RGBA'
+albedo_file_output.format.color_depth = args.color_depth
+links.new(alpha_albedo.outputs['Image'], albedo_file_output.inputs[0])
 
 # Delete default cube
-bpy.data.objects['Cube'].select = True
+context.active_object.select_set(True)
 bpy.ops.object.delete()
 
+# Import textured mesh
+bpy.ops.object.select_all(action='DESELECT')
+
 bpy.ops.import_scene.obj(filepath=args.obj)
-for object in bpy.context.scene.objects:
-    if object.name in ['Camera', 'Lamp']:
-        continue
-    bpy.context.scene.objects.active = object
-    if args.scale != 1:
-        bpy.ops.transform.resize(value=(args.scale,args.scale,args.scale))
-        bpy.ops.object.transform_apply(scale=True)
-    if args.remove_doubles:
-        bpy.ops.object.mode_set(mode='EDIT')
-        bpy.ops.mesh.remove_doubles()
-        bpy.ops.object.mode_set(mode='OBJECT')
-    if args.edge_split:
-        bpy.ops.object.modifier_add(type='EDGE_SPLIT')
-        bpy.context.object.modifiers["EdgeSplit"].split_angle = 1.32645
-        bpy.ops.object.modifier_apply(apply_as='DATA', modifier="EdgeSplit")
+
+obj = bpy.context.selected_objects[0]
+context.view_layer.objects.active = obj
+
+# Possibly disable specular shading
+for slot in obj.material_slots:
+    node = slot.material.node_tree.nodes['Principled BSDF']
+    node.inputs['Specular'].default_value = 0.05
+
+if args.scale != 1:
+    bpy.ops.transform.resize(value=(args.scale,args.scale,args.scale))
+    bpy.ops.object.transform_apply(scale=True)
+if args.remove_doubles:
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.mesh.remove_doubles()
+    bpy.ops.object.mode_set(mode='OBJECT')
+if args.edge_split:
+    bpy.ops.object.modifier_add(type='EDGE_SPLIT')
+    context.object.modifiers["EdgeSplit"].split_angle = 1.32645
+    bpy.ops.object.modifier_apply(modifier="EdgeSplit")
 
 # Make light just directional, disable shadows.
-lamp = bpy.data.lamps['Lamp']
-lamp.type = 'SUN'
-lamp.shadow_method = 'NOSHADOW'
+light = bpy.data.lights['Light']
+light.type = 'SUN'
+light.use_shadow = False
 # Possibly disable specular shading:
-lamp.use_specular = False
+light.specular_factor = 1.0
+light.energy = 10.0
 
 # Add another light source so stuff facing away from light is not completely dark
-bpy.ops.object.lamp_add(type='SUN')
-lamp2 = bpy.data.lamps['Sun']
-lamp2.shadow_method = 'NOSHADOW'
-lamp2.use_specular = False
-lamp2.energy = 0.015
-bpy.data.objects['Sun'].rotation_euler = bpy.data.objects['Lamp'].rotation_euler
+bpy.ops.object.light_add(type='SUN')
+light2 = bpy.data.lights['Sun']
+light2.use_shadow = False
+light2.specular_factor = 1.0
+light2.energy = 0.015
+bpy.data.objects['Sun'].rotation_euler = bpy.data.objects['Light'].rotation_euler
 bpy.data.objects['Sun'].rotation_euler[0] += 180
 
-
-def parent_obj_to_camera(b_camera):
-    origin = (0, 0, 0)
-    b_empty = bpy.data.objects.new("Empty", None)
-    b_empty.location = origin
-    b_camera.parent = b_empty  # setup parenting
-
-    scn = bpy.context.scene
-    scn.objects.link(b_empty)
-    scn.objects.active = b_empty
-    return b_empty
-
-
-scene = bpy.context.scene
-scene.render.resolution_x = 600
-scene.render.resolution_y = 600
-scene.render.resolution_percentage = 100
-scene.render.alpha_mode = 'TRANSPARENT'
+# Place camera
 cam = scene.objects['Camera']
 cam.location = (0, 1, 0.6)
+cam.data.lens = 35
+cam.data.sensor_width = 32
+
 cam_constraint = cam.constraints.new(type='TRACK_TO')
 cam_constraint.track_axis = 'TRACK_NEGATIVE_Z'
 cam_constraint.up_axis = 'UP_Y'
-b_empty = parent_obj_to_camera(cam)
-cam_constraint.target = b_empty
 
-model_identifier = os.path.split(os.path.split(args.obj)[0])[1]
-fp = os.path.join(args.output_folder, model_identifier, model_identifier)
-scene.render.image_settings.file_format = 'PNG'  # set output format to .png
+cam_empty = bpy.data.objects.new("Empty", None)
+cam_empty.location = (0, 0, 0)
+cam.parent = cam_empty
 
-from math import radians
+scene.collection.objects.link(cam_empty)
+context.view_layer.objects.active = cam_empty
+cam_constraint.target = cam_empty
 
 stepsize = 360.0 / args.views
 rotation_mode = 'XYZ'
+
+model_identifier = os.path.split(os.path.split(args.obj)[0])[1]
+fp = os.path.join(os.path.abspath(args.output_folder), model_identifier, model_identifier)
+ext = "exr" if args.format == 'OPEN_EXR' else "png"
 
 for output_node in [depth_file_output, normal_file_output, albedo_file_output]:
     output_node.base_path = ''
 
 for i in range(0, args.views):
-    print("Rotation {}, {}".format((stepsize * i), radians(stepsize * i)))
+    print("Rotation {}, {}".format((stepsize * i), math.radians(stepsize * i)))
 
-    scene.render.filepath = fp + '_r_{0:03d}'.format(int(i * stepsize))
-    depth_file_output.file_slots[0].path = scene.render.filepath + "_depth.png"
-    normal_file_output.file_slots[0].path = scene.render.filepath + "_normal.png"
-    albedo_file_output.file_slots[0].path = scene.render.filepath + "_albedo.png"
+    render_file_path = fp + '_r_{0:03d}'.format(int(i * stepsize))
+    depth_file_path = render_file_path + "_depth." + ext
+    normal_file_path = render_file_path + "_normal." + ext
+    albedo_file_path = render_file_path + "_albedo." + ext
+
+    scene.render.filepath = render_file_path
+    depth_file_output.file_slots[0].path = depth_file_path
+    normal_file_output.file_slots[0].path = normal_file_path
+    albedo_file_output.file_slots[0].path = albedo_file_path
 
     bpy.ops.render.render(write_still=True)  # render still
 
-    b_empty.rotation_euler[2] += radians(stepsize)
+    # remove frame number from file names
+    for n in [depth_file_path, normal_file_path, albedo_file_path]:
+        for n_old in glob(n+'*'):
+            os.rename(n_old, n)
+
+    cam_empty.rotation_euler[2] += math.radians(stepsize)
+
+bpy.ops.wm.save_as_mainfile(filepath='debug.blend')
